419cfa55e42e7782e572ddbc035cf9a5714b1531
Carlos Planchart <carlos_planchart@live.com>
2022-06-15 10:51:28 +0200

    Completed varios component

--- src/app/varios/varios.component.ts
+++ src/app/varios/varios.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-varios',
   templateUrl: './varios.component.html',
   styleUrls: ['./varios.component.css']
 })
-export class VariosComponent implements OnInit {
+export class VariosComponent implements OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
 
-  constructor() { }
+  constructor() {
+    console.log("Constructor");
+   }
 
   ngOnInit(): void {
+    console.log("On Init");
+  }
+  
+  ngDoCheck() {
+    console.log("Do check");
   }
 
+  ngAfterContentInit(): void {
+    console.log("After content Init")
+  }
+
+  ngAfterContentChecked(): void {
+    console.log("After Content Checked");
+  }
+
+  ngAfterViewInit(): void {
+    console.log("After View Init");
+  }
+
+  ngAfterViewChecked(): void {
+    console.log("After View Checked");
+  }
+  
+  ngOnDestroy(): void {
+    console.log("OnDestroy");
+  }
 }
